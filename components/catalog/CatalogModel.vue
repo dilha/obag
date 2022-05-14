a292a3c5bfbc8bff185d017bbd9a1b357f5b7ac8
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-05-14 15:56:00 +0600

    catalog subcategory titles were added

--- components/catalog/CatalogModel.vue
+++ components/catalog/CatalogModel.vue
@@ -12,17 +12,18 @@
       </div>
     </div>
     <swiper v-if="selectedCategory" ref="reviewsSlider" class="catalog__model-slider" :options="swiperOption">
-      <swiper-slide
-v-for="item in selectedCategory.subcategories" :key="item.id"
+      <swiper-slide v-for="item in selectedCategory.subcategories" :key="item.id"
         :class="['catalog__model-item', { 'is-active': selectedSubCategory === item.id }]" :data-id="item.id"
         @click.native.stop="handleToggleSubcategory(item)">
         <template v-if="item.preview_image">
-          <img :src="getImage(item)" alt="obag" />
-          
+          <div class="catalog__model-item-image">
+            <img :src="getImage(item)" alt="obag" />
+          </div>
+          <h5 class="catalog__model-item-title">{{ item.title }}</h5>
           <!-- <img v-if="selectedSubCategory === item.id" :src="item.preview_image" :alt="item.title" /> -->
           <!-- <img v-else :src="item.image" :alt="item.title" /> -->
         </template>
-        <img v-else src="@/assets/images/icons/obag.png" :alt="item.title" />
+        <img v-else class="catalog__model-item-cover-image" src="@/assets/images/icons/obag.png" :alt="item.title" />
       </swiper-slide>
     </swiper>
   </div>
@@ -71,9 +72,9 @@ export default {
       this.$emit('clear-filters')
     },
     getImage(item) {
-      if(item.preview_image) {
+      if (item.preview_image) {
         return item.preview_image
-      } 
+      }
       else {
         return require('@/' + 'assets/images/icons/obag.png')
       }
