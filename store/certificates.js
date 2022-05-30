f6a580ad262bd19271a1df2c8a7f0262d24a9646
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-05-30 15:28:47 +0600

    certificate card add to cart was fixed

--- store/certificates.js
+++ store/certificates.js
@@ -1,42 +1,32 @@
-
-export const state = ()=>({
+export const state = () => ({
   certificates: [],
-  error:null,
-  isLoading:false,
+  error: null,
+  isLoading: false,
 })
 
 export const mutationTypes = {
-  loadCertificatesSuccess:"mutation/certificatesSuccess load certificates success", 
+  loadCertificatesSuccess:
+    'mutation/certificatesSuccess load certificates success',
 }
 export const actionTypes = {
-  loadCertificates:'action/loadCertificates get all certificates',
+  loadCertificates: 'action/loadCertificates get all certificates',
 }
 
 export const mutations = {
-  [mutationTypes.loadCertificatesSuccess](state, payload){
-    state.certificates = payload;
+  [mutationTypes.loadCertificatesSuccess](state, payload) {
+    state.certificates = payload
   },
- 
 }
 export const actions = {
-  [actionTypes.loadCertificates]({commit}){
-
-    return new Promise(resolve => {
-      this.$api
-      .get('/certificates')
-      .then((response)=>{
-        const certificates = response.data.certificates;
-        commit(mutationTypes.loadCertificatesSuccess, certificates )
-        console.log(certificates)
-        resolve(certificates);
-        console.log(response)
+  [actionTypes.loadCertificates]({ commit }) {
+    return new Promise((resolve) => {
+      this.$api.get('/certificates').then((response) => {
+        const certificates = response.data.certificates
+        commit(mutationTypes.loadCertificatesSuccess, certificates)
+        resolve(certificates)
       })
-     
     })
   },
- 
 }
 
-export const getters = {
-
-}
+export const getters = {}
