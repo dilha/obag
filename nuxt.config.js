8cb9796e517b78274a8a40e2253d5f39466e36bc
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-06-14 19:12:40 +0600

    server configurations

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,8 +46,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-  server: {
-    port: 3001,
-    host: `0.0.0.0`,
-  },
+  //server: {
+  //  port: 3001,
+  //    host: `0.0.0.0`,
+  //},
 }
